db3f30e55cf0e73fca6095e74edfa619760b1e21
vidhyarabari <73937869+vidhyarabari@users.noreply.github.com>
2020-11-26 16:04:17 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,6 +7,11 @@ function setup() {
 movingrect = createSprite(20,10,100,50);
 fixedrect.shapeColor="green";
 movingrect.shapeColor="green";
+objectdemo1 = createSprite(300,400,40,40);
+objectdemo1.shapeColor="green";
+objectdemo2=createSprite(800,0,40,40);
+objectdemo2.velocityY=4;
+
 
 }
 
@@ -14,18 +19,26 @@ function draw() {
   background(255,255,255);
   movingrect.x=World.mouseX;
   movingrect.y=World.mouseY;
-  if (fixedrect.x-movingrect.x<fixedrect.width/2+movingrect.width/2 
-    && movingrect.x-fixedrect.x<fixedrect.width/2+movingrect.width/2
-     && fixedrect.y-movingrect.y<fixedrect.height/2+movingrect.height/2 
-     && movingrect.y-fixedrect.y<fixedrect.height/2+movingrect.height/2 )
+  if (istouching(movingrect,objectdemo1))
     {
-      fixedrect.shapeColor="red";
+      objectdemo1.shapeColor="red";
 movingrect.shapeColor="red";
     }
     else {
-      fixedrect.shapeColor="green";
+      objectdemo1.shapeColor="green";
 movingrect.shapeColor="green";
     }
+bounceoff(objectdemo2,fixedrect);
+    
   drawSprites();
 
+  
+
 }
+
+
+
+
+
+
+
